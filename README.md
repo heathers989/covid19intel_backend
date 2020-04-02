018d677fe17b089dec3eb0953f2fd160f7d33db4
Heather Spoelstra <hmspoelstra@gmail.com>
2020-04-01 20:09:14 -0400

    index and create routes working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # covid19intel_backend
 back end code for COVID-19 info tracker
+
+https://awwapp.com/b/ucchiv4su/#
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,9 +2,25 @@ const express = require("express");
 const comments = express.Router();
 const Comment = require("../models/usercomments.js");
 
+//index
+comments.get('/', (req, res) => {
+    Comment.find({}, (err, foundComments) => {
+      if (err) {
+        res.status(400).json({ error: err.message })
+      }
+      res.status(200).json(foundComments)
+    })
+  })
 
-
-
+//create
+comments.post('/', (req, res) => {
+    Comment.create(req.body, (error, createdComment) => {
+      if (error) {
+        res.status(400).json({ error: error.message })
+      }
+      res.status(200).json(createdComment) //  .json() will send proper headers in response so client knows it's json coming back
+    })
+  })
 
 
 
